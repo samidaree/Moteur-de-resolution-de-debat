4e42036394ae6a7470407d074df6d25a856d05f9
Sami <117381937+samidaree@users.noreply.github.com>
2022-11-11 23:31:37 +0100

    Fin Phase 1

--- src/up/sm/debat/Debat.java
+++ src/up/sm/debat/Debat.java
@@ -1,21 +1,48 @@
+
 package up.sm.debat;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Cette classe implémente les débats 
+ * Author : Sami BOUFASSA & Moncef BOUHABEL 
+ */
+
 public class Debat {
+
 	private Graph g ; 
 	private ArrayList <Integer> solutions; 
 	
+	/**
+	 * Class constructor 
+	 */
 	public Debat() {
 		this.g = new Graph(); 
 		solutions = new <String> ArrayList(); 
 	}
 	
+	/**
+	 * Saisit l'ordre du graphe 
+	 * @param nbSommets
+	 */
 	public void setG(int nbSommets) {
 		g.setNbSommets(nbSommets);
 	}
 	
+	public void afficherDebat() {
+		try {
+			System.out.println(this.g.afficherGraph()); 
+		}
+		catch (IOException e){
+			System.out.println("probleme affichage graphe"); 
+		}
+	}
+	
+	/**
+	 * Permet à l'utilisateur de saisir le nombre d'arguments / l'odre du graphe 
+	 */
 	public void saisieNbArguments() {
 		System.out.println("Saisissez le nombre d'arguments"); 
 		int x ; 
@@ -25,6 +52,9 @@ public class Debat {
 		//saisie.close();		
 	}
 	
+	/**
+	 * Permet à l'utilisateur d'ajouter les contradictions / arêtes du graphe 
+	 */
 	public void ajouterContradiction() {
 		System.out.println("Saisissez 2 arguments, le premier étant celui qui contredit le deuxième :"); 
 		Scanner saisie = new Scanner(System.in); 
@@ -33,6 +63,9 @@ public class Debat {
 		this.g.ajouterArete(Character.getNumericValue(s1.charAt(1)) -1, Character.getNumericValue(s2.charAt(1))-1);
 	}
 	
+	/**
+	 * Affiche le premier menu à l'utilisateur 
+	 */
 	public void menu1() {
 		int x ; 
 		Scanner saisie = new Scanner(System.in); 
@@ -40,41 +73,52 @@ public class Debat {
 		do {
 			System.out.println("Que souhaitez vous faire ? "); 
 			System.out.println("1) Ajouter une contradiction"); 
-			System.out.println("2) Fin"); 
+			System.out.println("2) Afficher le débat"); 
+			System.out.println("3) Fin"); 
 			
 			x = saisie.nextInt(); 
 			switch(x) {
 			case 1 : ajouterContradiction(); 
 				break ; 
-			case 2 : 
+			case 2 : afficherDebat(); 
 				break ; 
-			default : System.out.println("Vous devez saisir 1 ou 2 !") ; 
+			case 3 : 
+				break ; 
+			default : System.out.println("Vous devez saisir 1,2 ou 3 !") ; 
 			}
 			
-		} while (x!=2); 
+		} while (x!=3); 
 		
-
 		
 	}
 	
+	/**
+	 * Permet d'ajouter un argument dans l'ensemble des solutions 
+	 */
 	public void ajouterArgument() {
 		System.out.println("Ajoutez un argument dans la solution : ");
 		
 		Scanner saisie = new Scanner(System.in); 
 		String argument = saisie.nextLine(); 
 		this.solutions.add(Character.getNumericValue(argument.charAt(1))); 
+		afficherSolutions() ; 
 	}
 	
+	/**
+	 * Permet de retirer un argument de l'ensemble des solutions 
+	 */
 	public void retirerArgument() {
 		System.out.println("Saisissez l'argument que vous souhaitez retirer : ");
 		Scanner saisie = new Scanner(System.in); 
 		String argument = saisie.nextLine(); 
 		solutions.remove(Integer.valueOf(Character.getNumericValue(argument.charAt(1))));
+		afficherSolutions() ; 
 	}
 	
+	/**
+	 * Permet de vérifier si les arguments saisis par l'utilisateur forment une solution admissible
+	 */
 	public void verifierSolution() {
-		System.out.println("Voici la solution que vous avez rentré : "); 
-		System.out.println(this.solutions);
 		if (solutions.size()==0 || solutions.size()==1) {
 			System.out.println("Votre solution est admissible !"); 
 			return ; 
@@ -85,11 +129,15 @@ public class Debat {
 				// Si on trouve un sommet du graphe qui contredit un élément de l'ensemble S
 				if (g.getMatriceAdjacence()[j][solutions.get(i)-1] == 1) {
 					// Si ce sommet est aussi dans l'ensemble S
-					if (solutions.contains(j)) 
+					if (solutions.contains(j+1)) {
 						contradiction = true; 
+						//System.out.println("Contradiction interne" + j); 
+					}
+						
 					else {
 						contradiction = true ; 
 						for (int k = 0; k<solutions.size(); k++)
+							// Cas ou l'argument se défend contre celui qui l'a contredit en l'occurence k 
 							if (g.getMatriceAdjacence()[solutions.get(k)-1][j] == 1)
 								contradiction = false; 
 						
@@ -101,8 +149,31 @@ public class Debat {
 			System.out.println("Votre solution est admissible ! "); 
 		else 
 			System.out.println("Votre solution n'est pas admissible ! "); 
+		
+		afficherSolutions(); 
 	}
 	
+	/**
+	 * Permet d'afficher l'ensemble des solutions saisi par l'utilisateur
+	 */
+	public void afficherSolutions() {
+		System.out.println("Voici la solution que vous avez rentré : "); 
+		//System.out.println(this.solutions);
+		StringBuffer sb = new StringBuffer("{");
+		for (int i = 0; i<solutions.size();i++) {
+			if (i==solutions.size()-1)
+				sb.append("A").append(solutions.get(i)); 
+			else 
+				sb.append("A").append(solutions.get(i)).append(","); 
+		}
+		sb.append("}") ; 
+		System.out.println(sb); 
+		
+	}
+	
+	/**
+	 * Permet d'afficher le menu2 
+	 */
 	public void menu2() {
 	
 		int x ; 
--- src/up/sm/debat/Graph.java
+++ src/up/sm/debat/Graph.java
@@ -1,36 +1,100 @@
+
 package up.sm.debat;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Cette classe implémente les graphes en Java 
+ * @author Sami BOUFASSA & Moncef BOUHABEL 
+ */
 
 public class Graph {
 	private int nbSommets; 
 	private int [][] matriceAdjacence; 
 	
+	/**
+	 * Constructeur 
+	 * @param nbSommets Ordre du graphe 
+	 */
 	public Graph (int nbSommets) {
 		this.nbSommets = nbSommets; 
 		matriceAdjacence = new int [nbSommets][nbSommets]; 
 	}
 	
+	/**
+	 * Constructeur vide 
+	 */
 	public Graph() {
 		
 	}
-	
+	/**
+	 * Getter ordre du graphe 
+	 * @return le nombre de sommets du graphe 
+	 */
 	public int getNbSommets() {
 		return this.nbSommets; 
 	}
 	
+	/**
+	 * Getter matrice d'adjacence 
+	 * @return la matrice d'adjacence du graphe 
+	 */
 	public int[][] getMatriceAdjacence(){
 		return this.matriceAdjacence; 
 	}
 	
+	/**
+	 * Modifie l'ordre du graphe 
+	 * @param nbSommets nombre de sommets du graphe 
+	 */
 	public void setNbSommets(int nbSommets) {
 		this.nbSommets = nbSommets; 
 		matriceAdjacence = new int [nbSommets][nbSommets] ; 
 	}
 	
+	/**
+	 * Ajoute une arête dans le graphe 
+	 * @param s1 Sommet à la première extrémité de l'arête 
+	 * @param s2 Sommet à la deuxième extrémité de l'arête 
+	 */
 	public void ajouterArete(int s1, int s2) {
 		matriceAdjacence[s1][s2]= 1; 
 	}
 	
-	public void afficherGraph() {
+	/** 
+	 * Affiche le graphe 
+	 * @throws IOException 
+	 */
+	public String afficherGraph() throws IOException {
+		StringBuffer sb = new StringBuffer ("digraph debat {\n"); 
+		for (int i=0; i<nbSommets;i++) {
+			for (int j= 0; j<nbSommets; j++)
+				if(matriceAdjacence[i][j]==1) {
+					sb.append("A").append(i+1).append("->").append("A").append(j+1).append(";\n"); 
+				}
+		}
+		sb.append("}"); 
 		
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter("graph.txt")); 
+			bw.write(sb.toString());
+			bw.close(); 
+			
+	
+		}
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		return sb.toString(); 
 	}
 }
